d7c19735fe169e73cb2cca059517f01d457d2ae0
Achilles <brandongabucan@gmail.com>
2018-12-17 21:01:47 -0500

    save

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,12 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection(process.env.JAWSDB_URL);
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "",
+  database: "burger_db"
+});
 
 connection.connect(function (err) {
   if (err) {
